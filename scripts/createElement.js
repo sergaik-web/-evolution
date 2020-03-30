3cf638bb10da2f0f12c33d4c4969dd3eca5949e0
sergaik-web <seroga8863@gmail.com>
2020-03-30 21:11:06 +0300

    просто

--- scripts/createElement.js
+++ scripts/createElement.js
@@ -1,38 +1,40 @@
-const energy = document.getElementById('energy');
-import {arBacter, arEat} from "./main.js";
+const energy = document.getElementById("energy");
+import { arBacter, arEat } from "./main.js";
 
-class createElement{
-    constructor(itemClass, count, gridRow, gridColumn, background){
-        this.itemClass = itemClass;
-        this.count = count;
-        this.gridRow = gridRow ;
-        this.gridColumn = gridColumn;
-        this.background = background;
+class createElement {
+  constructor(itemClass, count, gridRow, gridColumn, background) {
+    this.itemClass = itemClass;
+    this.count = count;
+    this.gridRow = gridRow;
+    this.gridColumn = gridColumn;
+    this.background = background;
+  }
+  create() {
+    for (let i = this.count; i > 0; i--) {
+      let el = document.createElement("div");
+      el.className = this.itemClass;
+      if (this.count === 1 && this.itemClass === "bacter") {
+        el.style.gridRow = this.gridRow;
+        el.style.gridColumn = this.gridColumn;
+      } else if (this.count === 1 && this.itemClass === "eat") {
+        el.style.gridRow = this.gridRow;
+        el.style.gridColumn = this.gridColumn;
+        el.style.backgroundColor = this.background;
+      } else {
+        el.style.gridArea = `${Math.round(Math.random() * 100) + 1} / ${
+          Math.round(Math.random() * 100) + 1
+        }`;
+      }
+      petri.append(el);
+      if (this.itemClass === "bacter") {
+        el.energy = energy.value;
+        el.hungry = 2;
+        arBacter.push(el);
+      } else {
+        arEat.push(el);
+      }
     }
-    create() {
-        for (let i=this.count; i>0 ; i--){
-            let el = document.createElement('div');
-            el.className = this.itemClass;
-            if (this.count === 1 && this.itemClass === 'bacter'){
-                el.style.gridRow = this.gridRow;
-                el.style.gridColumn = this.gridColumn;
-            } else if (this.count === 1 && this.itemClass === 'eat'){
-                el.style.gridRow = this.gridRow;
-                el.style.gridColumn = this.gridColumn;
-                el.style.backgroundColor = this.background;
-            } else {
-                el.style.gridArea = `${Math.round(Math.random() * 100) + 1} / ${Math.round(Math.random() * 100) + 1}`;
-            }
-            petri.append(el);
-            if (this.itemClass==='bacter'){
-                el.energy = energy.value;
-                el.hungry = 2;
-                arBacter.push(el)
-            } else {
-                arEat.push(el);
-            }
-        }
-    }
+  }
 }
 
-export {createElement};
+export { createElement };
--- scripts/main.js
+++ scripts/main.js
@@ -1,20 +1,23 @@
 // Class
-import { createElement } from './createElement.js';
+import { createElement } from "./createElement.js";
 export { arBacter, arEat };
 
 // Variable
-const eatAll = document.getElementById('eat');
-const populAll = document.getElementById('popul');
-const petri = document.getElementById('petri');
-const stop = document.getElementById('stop');
-const clear = document.getElementById('clear');
-const time = document.getElementById('time');
-const countBacter = document.getElementById('countBacter');
-const countEat = document.getElementById('countEat');
+const eatAll = document.getElementById("eat");
+const populAll = document.getElementById("popul");
+const petri = document.getElementById("petri");
+const stop = document.getElementById("stop");
+const clear = document.getElementById("clear");
+const time = document.getElementById("time");
+const countBacter = document.getElementById("countBacter");
+const countEat = document.getElementById("countEat");
 let arBacter = [];
 let arEat = [];
 let count = 0;
-let dataAr = [['ход', 'еды', 'бактерий'], [0, 0, 0]];
+let dataAr = [
+  ["ход", "еды", "бактерий"],
+  [0, 0, 0],
+];
 
 // Function
 const eatDetected = (bacter) => {
@@ -24,13 +27,36 @@ const eatDetected = (bacter) => {
     const eatRow = parseInt(arEat[i].style.gridRow);
     const eatColumn = parseInt(arEat[i].style.gridColumn);
 
-    if ((eatRow === gridRow || eatRow - gridRow === -1 || eatRow - gridRow === 1) && ((eatColumn - gridColumn) === 1 || (eatColumn - gridColumn) === 2)) {
+    if (
+      (eatRow === gridRow ||
+        eatRow - gridRow === -1 ||
+        eatRow - gridRow === 1) &&
+      (eatColumn - gridColumn === 1 || eatColumn - gridColumn === 2)
+    ) {
       return 20;
-    } if ((eatRow === gridRow || eatRow - gridRow === -1 || eatRow - gridRow === 1) && ((eatColumn - gridColumn) === -1 || (eatColumn - gridColumn) === -2)) {
+    }
+    if (
+      (eatRow === gridRow ||
+        eatRow - gridRow === -1 ||
+        eatRow - gridRow === 1) &&
+      (eatColumn - gridColumn === -1 || eatColumn - gridColumn === -2)
+    ) {
       return 40;
-    } if ((eatColumn === gridColumn || eatColumn - gridColumn === 1 || eatColumn - gridColumn === -1) && ((eatRow - gridRow) === 1 || (eatRow - gridRow) === 2)) {
+    }
+    if (
+      (eatColumn === gridColumn ||
+        eatColumn - gridColumn === 1 ||
+        eatColumn - gridColumn === -1) &&
+      (eatRow - gridRow === 1 || eatRow - gridRow === 2)
+    ) {
       return 60;
-    } if ((eatColumn === gridColumn || eatColumn - gridColumn === 1 || eatColumn - gridColumn === -1) && ((eatRow - gridRow) === -1 || (eatRow - gridRow) === -1)) {
+    }
+    if (
+      (eatColumn === gridColumn ||
+        eatColumn - gridColumn === 1 ||
+        eatColumn - gridColumn === -1) &&
+      (eatRow - gridRow === -1 || eatRow - gridRow === -1)
+    ) {
       return 80;
     }
   }
@@ -41,7 +67,13 @@ const ripBacter = (bacter) => {
   if (arBacter.indexOf(bacter, 0) !== -1) {
     arBacter[arBacter.indexOf(bacter, 0)].remove();
     arBacter.splice(arBacter.indexOf(bacter, 0), 1);
-    const element = new createElement('eat', 1, bacter.style.gridRow, bacter.style.gridColumn, 'black');
+    const element = new createElement(
+      "eat",
+      1,
+      bacter.style.gridRow,
+      bacter.style.gridColumn,
+      "black"
+    );
     element.create();
   }
 };
@@ -51,7 +83,10 @@ const eating = (bacter) => {
   const gridColumn = parseInt(bacter.style.gridColumn);
 
   for (let i = 0; i < arEat.length; i++) {
-    if (parseInt(arEat[i].style.gridRow) === gridRow && parseInt(arEat[i].style.gridColumn) === gridColumn) {
+    if (
+      parseInt(arEat[i].style.gridRow) === gridRow &&
+      parseInt(arEat[i].style.gridColumn) === gridColumn
+    ) {
       arEat[i].remove();
       arEat.splice(i, 1);
       bacter.hungry--;
@@ -67,25 +102,35 @@ const moveBacter = (bacter) => {
   }
 
   if (bacter.hungry <= 0) {
-    const element = new createElement('bacter', 1, bacter.style.gridRow, bacter.style.gridColumn);
+    const element = new createElement(
+      "bacter",
+      1,
+      bacter.style.gridRow,
+      bacter.style.gridColumn
+    );
     element.create();
     bacter.hungry = 2;
   }
 
-
   let random = eatDetected(bacter);
-  if (random === -1) { random = Math.round(Math.random() * 100); }
+  if (random === -1) {
+    random = Math.round(Math.random() * 100);
+  }
   if (random < 25) {
     if (parseInt(bacter.style.gridColumn) === 100) {
       bacter.style.gridColumn = `${1}/ auto`;
     } else {
-      bacter.style.gridColumn = `${parseInt(bacter.style.gridColumn) + 1}/ auto`;
+      bacter.style.gridColumn = `${
+        parseInt(bacter.style.gridColumn) + 1
+      }/ auto`;
     }
   } else if (random >= 25 && random < 50) {
     if (parseInt(bacter.style.gridColumn) === 1) {
       bacter.style.gridColumn = `${100}/ auto`;
     } else {
-      bacter.style.gridColumn = `${parseInt(bacter.style.gridColumn) - 1}/ auto`;
+      bacter.style.gridColumn = `${
+        parseInt(bacter.style.gridColumn) - 1
+      }/ auto`;
     }
   } else if (random >= 50 && random < 75) {
     if (parseInt(bacter.style.gridRow) === 100) {
@@ -109,43 +154,47 @@ const timer = () => {
   countBacter.textContent = arBacter.length;
   countEat.textContent = arEat.length;
 
-  if (time.value === '0') { return; }
+  if (time.value === "0") {
+    return;
+  }
 
-  const bacterAll = document.querySelectorAll('.bacter');
+  const bacterAll = document.querySelectorAll(".bacter");
 
   for (let i = bacterAll.length - 1; i >= 0; i--) {
     moveBacter(bacterAll[i]);
   }
 
   if (Math.round(Math.random() * 100) > 80) {
-    const element = new createElement('eat', 2);
+    const element = new createElement("eat", 2);
     element.create();
   }
 
   count++;
   dataAr.push([count, arEat.length, arBacter.length]);
-  if (count % 10 === 0) { google.charts.setOnLoadCallback(drawChart); }
+  if (count % 10 === 0) {
+    google.charts.setOnLoadCallback(drawChart);
+  }
 
-  setTimeout(() => { timer(time.value); }, time.value);
+  setTimeout(() => {
+    timer(time.value);
+  }, time.value);
 };
 
-
-
 // EventListener
-document.addEventListener('submit', (event) => {
+document.addEventListener("submit", (event) => {
   event.preventDefault();
-  if (document.querySelectorAll('.bacter').length > 0) {
+  if (document.querySelectorAll(".bacter").length > 0) {
     life();
   } else {
-    let element = new createElement('bacter', populAll.value);
+    let element = new createElement("bacter", populAll.value);
     element.create();
-    element = new createElement('eat', eatAll.value);
+    element = new createElement("eat", eatAll.value);
     element.create();
     life();
   }
 });
 
-clear.addEventListener('click', () => {
+clear.addEventListener("click", () => {
   function clearPetri() {
     const elements = petri.children;
     for (let i = elements.length - 1; i >= 0; i--) {
@@ -156,28 +205,32 @@ clear.addEventListener('click', () => {
   arBacter = [];
   arEat = [];
   count = 0;
-  dataAr = [['ход', 'еды', 'бактерий'], [0, 0, 0]];
+  dataAr = [
+    ["ход", "еды", "бактерий"],
+    [0, 0, 0],
+  ];
 });
 
-stop.addEventListener('click', () => {
+stop.addEventListener("click", () => {
   time.value = 0;
 });
-
 
 // Google Chart
 const drawChart = () => {
   const data = google.visualization.arrayToDataTable(dataAr);
   // Define the chart to be drawn.
   const options = {
-    title: 'Отношение еды к популяции',
-    legend: { position: 'bottom' },
+    title: "Отношение еды к популяции",
+    legend: { position: "bottom" },
   };
 
   // Instantiate and draw the chart.
-  const chart = new google.visualization.LineChart(document.getElementById('myPieChart'));
+  const chart = new google.visualization.LineChart(
+    document.getElementById("myPieChart")
+  );
   chart.draw(data, options);
 };
 
-google.charts.load('current', { packages: ['corechart'], language: 'ru' });
+google.charts.load("current", { packages: ["corechart"], language: "ru" });
 
 google.charts.setOnLoadCallback(drawChart);
